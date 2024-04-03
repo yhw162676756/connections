df0b0712aee366e59a7370a2247ba62188545649
yhw162676756 <57939013+yhw162676756@users.noreply.github.com>
2024-04-04 01:36:33 +0800

    Delete script directory

--- script/updFileList.py
+++
@@ -1,9 +0,0 @@
-# 将./data/的文件列表写入./data/fileList中
-import os
-path = './data/'
-fileList = os.listdir(path)
-# print(fileList)
-fileList.remove('fileList')
-with open(path + 'fileList', 'w') as f:
-    for file in fileList:
-        f.write(file + '\n')
