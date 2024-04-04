dc44aa806f9d9feddc18c4ecf4c4c99547ef4686
yhw162676756 <806645943@qq.com>
2024-04-04 23:36:54 +0800

    4.5

--- script/updFileList.py
+++ script/updFileList.py
@@ -1,9 +1,33 @@
-# 将./data/的文件列表写入./data/fileList中
 import os
+import requests
 path = './data/'
-fileList = os.listdir(path)
-# print(fileList)
-fileList.remove('fileList')
-with open(path + 'fileList', 'w') as f:
-    for file in fileList:
-        f.write(file + '\n')
+fileListPath = path + 'fileList'
+oldFileSet = set()
+if os.path.exists(fileListPath):
+    with open(fileListPath, 'r') as f:
+        for line in f:
+            oldFileSet.add(line.strip())
+newfileSet = set(os.listdir(path))
+newfileSet.remove('fileList')
+addedfileSet = newfileSet - oldFileSet
+if not addedfileSet:
+    print('No new file added.')
+    exit()
+with open(fileListPath, 'a') as f:
+    for file in addedfileSet:
+        f.write(file+'\n')
+print('File list updated.')
+
+# url = 'https://connects-update-zwpgxvhxeg.cn-hangzhou.fcapp.run'
+# 向url post文件
+# for file in addedfileSet:
+#     with open(path+file, 'rb') as f:
+#         data = f.read()
+#         res = requests.post(url, data=data)
+#         print(res.text)
+# with open(fileListPath, 'rb') as f:
+#     data = f.read()
+#     # res = requests.post(url, data=data)
+#     res = requests.post(url, data=data)
+#     print(res.text)
+# print('All files uploaded.')
